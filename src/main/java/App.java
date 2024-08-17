8c67b63e7d4e236b8197261f0fb8ab5a22bfda87
FlevieLans <flownart@gmail.com>
2024-08-17 19:34:16 +0300

    Практическая задача 2.1.2 Spring. Первые шаги. HelloWorld.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat = applicationContext.getBean("catBean", Cat.class);
+        Cat cat2 = applicationContext.getBean("catBean", Cat.class);
+
+        System.out.println("bean = bean2 ? " + (bean == bean2));
+        System.out.println("cat = cat2 ? " + (cat == cat2));
+
         System.out.println(bean.getMessage());
     }
 }
